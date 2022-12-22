3434b5e6265d1d426212c2a99ec6e772c65c1b9d
Miles Wu <slion0725@gmail.com>
2022-12-22 11:31:29 +0800

    upd

--- ent/schema/todo.go
+++ ent/schema/todo.go
@@ -1,6 +1,12 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
 
 // Todo holds the schema definition for the Todo entity.
 type Todo struct {
@@ -9,7 +15,30 @@ type Todo struct {
 
 // Fields of the Todo.
 func (Todo) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.Text("text").
+			NotEmpty().
+			Annotations(
+				entgql.OrderField("TEXT"),
+			),
+
+		field.Enum("status").
+			NamedValues(
+				"InProgress", "IN_PROGRESS",
+				"Completed", "COMPLETED",
+			).
+			Default("IN_PROGRESS").
+			Annotations(
+				entgql.OrderField("STATUS"),
+			),
+
+		field.Time("created_at").
+			Default(time.Now).
+			Immutable().
+			Annotations(
+				entgql.OrderField("CREATED_AT"),
+			),
+	}
 }
 
 // Edges of the Todo.
--- history.md
+++ history.md
@@ -7,6 +7,12 @@ git init -b main
 go mod init go-ent-gqlgen
 # 安裝ent
 go get entgo.io/ent/cmd/ent
+# 安裝entgql
+go get entgo.io/contrib/entgql
 # 建立Todo的Model
 go run -mod=mod entgo.io/ent/cmd/ent init Todo
+# 定義欄位,Fields()
+open ent/schema/todo.go
+# 定義欄位產生相關文件
+go generate ./ent
 ```
